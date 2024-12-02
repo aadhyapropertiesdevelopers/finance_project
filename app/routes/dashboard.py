bd8f6304148f589635388d93914e974cf19d2cc2
Musipatla Akhila <akhilareddymusipatla910@gmail.com>
2024-12-02 18:29:58 +0530

    integrated login page

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -4,11 +4,20 @@ from ..models.user import User
 from .. import db
 from werkzeug.security import generate_password_hash, check_password_hash  # Import check_password_hash
 from app.services.jwt_utils import encode_jwt
-
+from flask import url_for
 
 bp = Blueprint('dashboard', __name__)
-# Route for the Dashboard page
 @bp.route('/')
+def login_page():
+    return render_template('index.html')
+# Route for the Dashboard page
+
+
+@bp.route('/signup')
+def signuppage():
+    return render_template('index.html')
+
+@bp.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')  # Serve the main dashboard page
 #Route for submitting the payment form
